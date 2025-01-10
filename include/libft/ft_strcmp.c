51dc86c9c18870a91727ab9c7ea923b66bd8c312
Miguel Teixeira Nunes <miteixei@c1r13s2.42lisboa.com>
2025-01-10 18:55:32 +0000

    renamed the function in the file strcmp, which I forgot to prior

--- include/libft/ft_strcmp.c
+++ include/libft/ft_strcmp.c
@@ -6,13 +6,13 @@
 /*   By: suroh <suroh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 15:25:36 by suroh             #+#    #+#             */
-/*   Updated: 2025/01/09 17:41:51 by miteixei         ###   ########.fr       */
+/*   Updated: 2025/01/10 16:25:45 by miteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t	i;
 
