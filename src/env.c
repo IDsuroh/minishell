354ed6c7f467e3f8093c5d7144a870eb593a9e65
suroh <suroh@student.42lisboa.com>
2025-03-04 20:18:34 +0000

    changed and split env.c files according to the norminette

--- src/env.c
+++ src/env.c
@@ -6,7 +6,7 @@
 /*   By: miteixei <miteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 20:42:03 by miteixei          #+#    #+#             */
-/*   Updated: 2025/03/03 18:09:13 by miteixei         ###   ########.fr       */
+/*   Updated: 2025/03/04 20:15:57 by suroh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,6 @@
 //   and then each time one wants to add, modify, or remove a variable
 //   a copy of the double char pointer is to be created, unless no change
 //   is going to occur.
-
-// Print the list of strings line by line.
-void	print_vars(char **envp)
-{
-	while (*envp)
-		ft_putstr_fd(*(envp++), 1);
-}
 
 char	**make_envp(t_list_header *header)
 {
@@ -37,7 +30,7 @@ char	**make_envp(t_list_header *header)
 	while (elm)
 	{
 		envp[i] = malloc((ft_strlen(elm->key) + 1 + ft_strlen(elm->value) + 1)
-			* sizeof(char));
+				* sizeof(char));
 		ft_strlcpy(envp[i], elm->key, ft_strlen(elm->key) + 1);
 		ft_strlcpy(&envp[i][ft_strlen(elm->key)], "=", 2);
 		ft_strlcpy(&envp[i][ft_strlen(elm->key) + 1], elm->value,
@@ -47,65 +40,6 @@ char	**make_envp(t_list_header *header)
 	}
 	envp[i] = NULL;
 	return (envp);
-}
-
-t_var_elm	*create_var(char *key, char *value)
-{
-	t_var_elm	*var;
-
-	if (!key)
-		return (NULL);
-	var = malloc(sizeof(t_var_elm));
-	var->key = key;
-	var->value = value;
-	var->prev = NULL;
-	var->next = NULL;
-	return (var);
-}
-
-void	add_var(t_list_header *header, t_var_elm *var)
-{
-	t_var_elm	*elm;
-
-	elm = get_value(header, var->key);
-	if (!elm)
-	{
-		var = header->tail->next;
-		header->tail = var;
-	}
-	else
-	{
-		elm->value = ft_strdup(var->value);
-		del_var(var);
-	}
-}
-
-void	del_var(t_var_elm *var)
-{
-	free(var->key);
-	free(var->value);
-	free(var);
-}
-
-void	rem_var(t_list_header *header, t_var_elm *var)
-{
-	t_var_elm	*prev;
-	t_var_elm	*next;
-
-	if (!var)
-		return ;
-	prev = var->prev;
-	next = var->next;
-	if (prev)
-		prev->next = next;
-	if (next)
-		next->prev = prev;
-	if (var == header->head)
-		header->head = var->next;
-	if (var == header->tail)
-		header->tail = var->prev;
-	del_var(var);
-	--(header->size);
 }
 
 t_var_elm	*extract_var(char *var)
