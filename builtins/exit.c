62960d1fbf6f0e783a26d1b3cb3283d9f06fd6d7
IDsuroh <roh7891backup@gmail.com>
2025-03-23 22:33:45 +0000

    almost there

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: miteixei <miteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 19:10:17 by miteixei          #+#    #+#             */
-/*   Updated: 2025/03/19 17:54:02 by suroh            ###   ########.fr       */
+/*   Updated: 2025/03/23 22:09:57 by suroh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ void	_exit_(t_almighty *mighty, int *var_exit)
 		exit_val = *var_exit;
 	else
 		exit_val = mighty->exit_stat;
-	// FREE EVERYTHING
 	free_all(mighty);
-	free(mighty);
 	exit(exit_val);
 }
 
--- execution/execute_builtin.c
+++ execution/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: suroh <suroh@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 20:35:16 by suroh             #+#    #+#             */
-/*   Updated: 2025/03/19 20:58:10 by suroh            ###   ########.fr       */
+/*   Updated: 2025/03/23 22:18:59 by suroh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,9 @@ static int	apply_builtin_redir(t_simple_cmd *cmd)
 	return (saved_stdout);
 }
 
-int	execute_builtin(t_simple_cmd *cmd, t_almighty *mighty)
+static void	dispatch_builtin(t_simple_cmd *cmd, t_almighty *mighty,
+			int saved_stdout)
 {
-	int	saved_stdout;
-	int	status;
-
-	saved_stdout = apply_builtin_redir(cmd);
-	if (saved_stdout < 0)
-		return (-1);
 	if (ft_strcmp(cmd->argv[0], "echo") == 0)
 		_echo(cmd->argv + 1);
 	else if (ft_strcmp(cmd->argv[0], "cd") == 0)
@@ -59,9 +54,24 @@ int	execute_builtin(t_simple_cmd *cmd, t_almighty *mighty)
 	else if (ft_strcmp(cmd->argv[0], "env") == 0)
 		_env(mighty);
 	else if (ft_strcmp(cmd->argv[0], "exit") == 0)
+	{
+		close(saved_stdout);
 		_exit_(mighty, NULL);
+	}
+}
+
+int	execute_builtin(t_simple_cmd *cmd, t_almighty *mighty)
+{
+	int	saved_stdout;
+	int	status;
+
+	saved_stdout = apply_builtin_redir(cmd);
+	if (saved_stdout < 0)
+		return (-1);
+	dispatch_builtin(cmd, mighty, saved_stdout);
 	status = mighty->exit_stat;
-	dup2(saved_stdout, STDOUT_FILENO);
+	if (dup2(saved_stdout, STDOUT_FILENO) < 0)
+		perror("dup2");
 	close(saved_stdout);
 	return (status);
 }
